d9e807ae439f8b5b88982d847c63acdeea3fc122
Parwiz-Haqbien <Prwiz.jan03@gmail.com>
2023-01-03 15:21:34 +1100

    added the start function

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ const maxHour = 17;
 var currentDay = moment().format("dddd, MMMM Do");
 $("#currentDay").text(currentDay);
 
-function classSelection(hour) {
+function colorCode(hour) {
   var currentTime = moment().hour();
   if (hour < currentTime) {
     return "past";
@@ -20,3 +20,20 @@ function save(schedule) {
     memo[`memo${schedule.data.hour}`] = $(`#${schedule.data.hour}`)[0].value;
     localStorage.setItem("memo", JSON.stringify(memo));
   }
+
+
+  function start(){
+    const memo = JSON.parse(localStorage.memo);
+    for (var i = minHour; i <= maxHour; i += 1) {
+      if(memo[`memo${i}`]){
+        $(`#${i}`)[0].innerHTML = memo[`memo${i}`];
+      }
+    }
+  }
+  
+  for (var i = minHour; i <= maxHour; i += 1) {
+    $(`#${i}`).addClass(colorCode(i));
+    $(`#${i}~button`).click({ hour: i }, save);
+  }
+  
+  start();
